aae53ee6e6898bda2c50a0d7738d09047d6249b9
Zach Coyle <zach.coyle@gmail.com>
2024-09-12 17:31:53 -0400

    issues w/ sqlx, but almost compiles

--- flake.nix
+++ flake.nix
@@ -1,71 +1,118 @@
 {
-  description = "Build a cargo project without extra checks";
-
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    crane.url = "github:ipetkov/crane";
-    flake-utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
-
   outputs =
-    {
-      self,
-      nixpkgs,
-      crane,
-      flake-utils,
-      ...
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
+    inputs@{ nixpkgs, flake-parts, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
+      imports = [ ];
+      perSystem =
+        { pkgs, ... }:
+        let
+          rain' =
+            {
+              pkgs,
+              lib,
+              gn,
+              rustPlatform,
+              clangStdenv,
+              ninja,
+              fetchFromGitHub,
+              linkFarm,
+              fetchgit,
+              runCommand,
+              freetype,
+              fontconfig,
+              ...
+            }:
+            let
+              src = ./.;
+              srcMigrations = src + /migrations;
 
-        buildInputs = with pkgs; [
-          openssl
-          libGL
-          libxkbcommon
-          vulkan-loader
-          wayland
-          wayland-protocols
-        ];
+              sqlx-db =
+                runCommand "sqlx-db-prepare"
+                  {
+                    nativeBuildInputs = [ pkgs.sqlx-cli ];
+                  }
+                  ''
+                    mkdir $out
+                    export DATABASE_URL=sqlite:$out/db.sqlite3
+                    sqlx database create
+                    sqlx migrate run --source ${srcMigrations} 
+                  '';
+            in
+            rustPlatform.buildRustPackage.override { stdenv = clangStdenv; } rec {
+              pname = "rain";
+              version = "unstable";
+              src = ./.;
+              cargoLock = {
+                lockFile = ./Cargo.lock;
+                outputHashes = {
+                  "morphorm-0.6.4" = "sha256-JZ49mB44q/EQbNMdflcnJVNjbnY0dg6+gAjVX4mDhJg=";
+                  "selectors-0.23.0" = "sha256-9nD2YY9Z9YDrQqy99T02FCC5Q7oGjJamPP/ciTmCkUc=";
+                };
+              };
 
-        rain = crane.lib.${system}.buildPackage {
-          src = ./.;
-          inherit buildInputs;
+              SKIA_SOURCE_DIR =
+                let
+                  repo = fetchFromGitHub {
+                    owner = "rust-skia";
+                    repo = "skia";
+                    # see rust-skia:skia-bindings/Cargo.toml#package.metadata skia
+                    rev = "m126-0.74.2";
+                    hash = "sha256-4l6ekAJy+pG27hBGT6A6LLRwbsyKinJf6PP6mMHwaAs=";
+                  };
+                  # The externals for skia are taken from skia/DEPS
+                  externals = linkFarm "skia-externals" (
+                    lib.mapAttrsToList (name: value: {
+                      inherit name;
+                      path = fetchgit value;
+                    }) (lib.importJSON ./skia-externals.json)
+                  );
+                in
+                runCommand "source" { } ''
+                  cp -R ${repo} $out
+                  chmod -R +w $out
+                  ln -s ${externals} $out/third_party/externals
+                '';
+              SKIA_GN_COMMAND = "${gn}/bin/gn";
+              SKIA_NINJA_COMMAND = "${ninja}/bin/ninja";
 
-          nativeBuildInputs = with pkgs; [
-            gtk-layer-shell
-            gtk3
-            pkg-config
-          ];
+              buildInputs = with pkgs; [
+                openssl
+                rustPlatform.bindgenHook
+                freetype
+                fontconfig
+              ];
+
+              nativeBuildInputs = with pkgs; [
+                python3
+                pkg-config
+              ];
+
+              disallowedReferences = [ SKIA_SOURCE_DIR ];
+
+              # # FIXME: this needs modified for rustPlatform.buildRustPackage
+              # overrideMain = old: {
+              linkDb = ''
+                export DATABASE_URL=sqlite:${sqlx-db}/db.sqlite3     
+              '';
+
+              preBuildPhases = [ "linkDb" ];
+              # };
+            };
+
+          rain = pkgs.callPackage rain' { };
+        in
+        {
+          packages.default = rain;
+
+          devShells.default = pkgs.mkShell {
+            buildInputs = [ ];
+            inputsFrom = [ rain ];
+          };
         };
-      in
-      {
-        checks = {
-          my-crate = rain;
-        };
-
-        packages = {
-          inherit rain;
-          default = rain;
-        };
-
-        apps.default = flake-utils.lib.mkApp { drv = rain; };
-
-        devShells.default = pkgs.mkShell {
-          buildInputs = with pkgs; [
-            cargo
-            rustc
-            openssl
-            pkg-config
-            sqlx-cli
-          ];
-
-          inputsFrom = builtins.attrValues self.checks;
-
-          LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
-          # Extra inputs can be added here
-        };
-      }
-    );
+    };
 }
