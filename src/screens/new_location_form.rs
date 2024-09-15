a4c4c6b716695b5c0acc4267ca32ef0f7ad0335b
Zach Coyle <zach.coyle@gmail.com>
2024-09-15 19:59:36 -0400

    validation partially implemented

--- src/screens/new_location_form.rs
+++ src/screens/new_location_form.rs
@@ -1,5 +1,6 @@
 use anyhow::Error;
 use pollster::FutureExt as _;
+use validator::{Validate, ValidationError, ValidationErrors};
 use vizia::prelude::*;
 
 use super::{app_data::AppEvent, queries::add_location_to_db};
@@ -11,45 +12,60 @@ pub enum NewLocationFormEvent {
   FormError(Error),
 }
 
-#[derive(Default, Debug, Lens, Clone)]
-pub struct NewLocationFormData {
+#[derive(Default, Debug, Lens, Clone, Validate)]
+struct FormState {
+  #[validate(custom(function = "validate_geohash"))]
   pub geohash: String,
+  #[validate(length(min = 1))]
   pub name: String,
-  pub valid: bool,
   pub submitting: bool,
+  pub show_validation_errors: bool,
+  pub validation_errors: Option<ValidationErrors>,
   pub error_message: Option<String>,
 }
 
-fn validate(form_data: NewLocationFormData) -> bool {
-  form_data.name != ""
-    && form_data.geohash != ""
-    && geohash::decode(&form_data.geohash)
-      .ok()
-      .is_some()
+fn validate_geohash(geohash: &str) -> Result<(), ValidationError> {
+  match geohash::decode(geohash) {
+    Ok(_) => Ok(()),
+    Err(_) => Err(ValidationError::new("Invalid Geohash")),
+  }
 }
 
-impl Model for NewLocationFormData {
+// fn validate(form_data: NewLocationFormData) -> bool {
+//   form_data.name != ""
+//     && form_data.geohash != ""
+//     && geohash::decode(&form_data.geohash)
+//       .ok()
+//       .is_some()
+// }
+
+impl Model for FormState {
   fn event(&mut self, cx: &mut EventContext, event: &mut Event) {
     event.map(
       |new_location_form_event, _meta| match new_location_form_event {
         NewLocationFormEvent::SetName(name) => {
           println!("NewLocationFormEvent::SetName({:#?})", name);
           self.name = name.to_string();
-          self.valid = validate(self.clone());
+          // self.valid = validate(self.clone());
           println!("New State: {:#?}", self);
         }
 
         NewLocationFormEvent::SetGeohash(geohash) => {
           println!("NewLocationFormEvent::SetGeohash({:#?})", geohash);
           self.geohash = geohash.to_string();
-          self.valid = validate(self.clone());
+          self.validation_errors = self
+            .validate()
+            .err();
           println!("New State: {:#?}", self);
         }
 
         NewLocationFormEvent::Submit => {
           println!("NewLocationFormEvent::Submit");
           self.submitting = true;
-          if self.valid {
+          if self
+            .validation_errors
+            .is_none()
+          {
             match add_location_to_db(&self.name, &self.geohash).block_on() {
               Ok(()) => cx.emit(AppEvent::ConfirmLocation(
                 self
@@ -82,34 +98,32 @@ pub struct NewLocationForm {}
 impl NewLocationForm {
   pub fn new(cx: &mut Context) -> Handle<Self> {
     Self {}.build(cx, |cx| {
-      NewLocationFormData::default().build(cx);
-      Binding::new(cx, NewLocationFormData::error_message, |cx, lens| {
+      FormState::default().build(cx);
+      Binding::new(cx, FormState::error_message, |cx, lens| {
         if let Some(error_message) = lens.get(cx) {
           Label::new(cx, format!("Error: {}", error_message)).class("error");
         }
       });
       VStack::new(cx, |cx| {
         HStack::new(cx, |cx| {
-          Textbox::new(cx, NewLocationFormData::geohash)
+          Textbox::new(cx, FormState::geohash)
             .on_edit(|ex, geohash| {
               ex.emit(NewLocationFormEvent::SetGeohash(geohash));
             })
             .class("form_input");
-          Textbox::new(cx, NewLocationFormData::name)
+          Textbox::new(cx, FormState::name)
             .on_edit(|ex, name| {
               ex.emit(NewLocationFormEvent::SetName(name));
             })
             .class("form_input");
         })
         .class("row");
-        Binding::new(cx, NewLocationFormData::valid, |cx, lens| {
-          let valid = lens.get(cx);
-          Button::new(cx, |cx| Label::new(cx, "Save Location"))
-            .on_press(|ex| {
-              ex.emit(NewLocationFormEvent::Submit);
-            })
-            .disabled(!valid);
-        });
+
+        Button::new(cx, |cx| Label::new(cx, "Save Location"))
+          .on_press(|ex| {
+            ex.emit(NewLocationFormEvent::Submit);
+          })
+          .disabled(false);
       })
       .class("col");
     })
